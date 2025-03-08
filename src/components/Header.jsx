70973cf93188934e617dfc14fe6d04e538d253c4
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2025-03-08 13:37:49 +0530

    Header complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,80 @@
+import { Button, ConfigProvider } from "antd";
+import { createStyles } from "antd-style";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const useStyle = createStyles(({ prefixCls, css }) => ({
+  linearGradientButton: css`
+    &.${prefixCls}-btn-primary:not([disabled]):not(
+        .${prefixCls}-btn-dangerous
+      ) {
+      > span {
+        position: relative;
+      }
+
+      &::before {
+        content: "";
+        background: linear-gradient(
+          43deg,
+          #4158d0 0%,
+          #c850c0 46%,
+          #ffcc70 100%
+        );
+        position: absolute;
+        inset: -1px;
+        opacity: 1;
+        transition: all 0.5s;
+        border-radius: inherit;
+      }
+
+      &:hover::before {
+        opacity: 0;
+      }
+    }
+  `,
+}));
+
 export default function Header() {
+  const location = useLocation();
+  const [activePath, setActivePath] = useState(location.pathname);
+
+  const { styles } = useStyle();
+
+  useEffect(() => {
+    setActivePath(location.pathname);
+  }, [location.pathname]);
+
   return (
-    <header className="bg-bg flex justify-center gap-5 p-5">
-      <img src="./logo.webp" alt="Wylo Logo" className="h-10" />
-      <h1 className="text-accent text-center font-mono text-3xl font-bold uppercase">
-        Assignment
-      </h1>
+    <header className="fixed top-0 left-0 z-50 w-full p-8">
+      <div className="container mx-auto flex items-center justify-between px-10 select-none">
+        <Link to="/">
+          <img src="./logo.webp" alt="Wylo Logo" className="h-8" />
+        </Link>
+        <Link to={activePath == "/" ? "/new" : "/"}>
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary: "#4158D0",
+                borderRadiusLG: 24,
+              },
+              components: {
+                Button: {
+                  fontWeight: 600,
+                  paddingBlockLG: 12,
+                  paddingInlineLG: 28,
+                },
+              },
+            }}
+            button={{
+              className: styles.linearGradientButton,
+            }}
+          >
+            <Button type="primary" size="large">
+              {activePath == "/" ? "New Post" : "Home"}
+            </Button>
+          </ConfigProvider>
+        </Link>
+      </div>
     </header>
   );
 }
