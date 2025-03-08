ffa4dccfcfd8220ac79433b76b47060e2a6ce910
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2025-03-08 23:17:31 +0530

    Add functionality to add image URL

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,4 +1,5 @@
 import { Button, ConfigProvider, Input } from "antd";
+import { useState } from "react";
 
 export default function FormComponent({
   handlerFn,
@@ -11,6 +12,7 @@ export default function FormComponent({
   styles,
 }) {
   let imgURL = typeof img == "string" ? img : URL.createObjectURL(img);
+  const [isURL, setIsURL] = useState(false);
   return (
     <section className="flex h-dvh items-center justify-center">
       <div className="flex w-2/3 flex-col gap-8 rounded-2xl border-2 border-white p-5 shadow-lg shadow-[#ffffff99] select-none sm:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6">
@@ -30,8 +32,10 @@ export default function FormComponent({
               className="mx-auto h-48 w-fit rounded-2xl border-2 border-white object-contain p-2 shadow-lg shadow-[#ffffff99]"
             />
           ) : (
-            <div className="mx-auto flex size-48 items-center justify-center rounded-2xl border-2 border-white p-2 font-mono shadow-lg shadow-[#ffffff99]">
-              Upload an image
+            <div className="mx-auto flex size-48 flex-col items-center justify-center rounded-2xl border-2 border-white p-2 font-mono shadow-lg shadow-[#ffffff99]">
+              <span>Upload an image</span>
+              <span>or</span>
+              <span>enter an URL</span>
             </div>
           )}
 
@@ -41,20 +45,57 @@ export default function FormComponent({
             onChange={(e) => setCaption(e.target.value)}
             value={caption}
           />
-          <Button type="primary" size="large">
-            <label htmlFor="fileInput" className="w-full">
-              Upload
-            </label>
-          </Button>
-          <input
-            type="file"
-            className="hidden"
-            id="fileInput"
-            name="fileInput"
-            onChange={(e) => {
-              setImg(e.target.files[0]);
-            }}
-          />
+          <div className="flex flex-col gap-2">
+            <ConfigProvider
+              theme={{
+                components: {
+                  Button: {
+                    fontWeight: 600,
+                    paddingBlockLG: 12,
+                    paddingInlineLG: 28,
+                  },
+                },
+              }}
+              button={{
+                className: styles.linearGradientButton,
+              }}
+            >
+              <Button
+                type="primary"
+                size="large"
+                onClick={() => {
+                  setIsURL(!isURL);
+                  setImg("");
+                }}
+              >
+                URL or Upload
+              </Button>
+            </ConfigProvider>
+            <Input
+              placeholder="Image URL"
+              disabled={`${isURL ? "" : "hidden"}`}
+              onChange={(e) => setImg(e.target.value)}
+              value={img}
+            />
+            <Button
+              type="primary"
+              size="large"
+              disabled={`${isURL ? "hidden" : ""}`}
+            >
+              <label htmlFor="fileInput" className="w-full">
+                Upload
+              </label>
+            </Button>
+            <input
+              type="file"
+              className="hidden"
+              id="fileInput"
+              name="fileInput"
+              onChange={(e) => {
+                setImg(e.target.files[0]);
+              }}
+            />
+          </div>
 
           <ConfigProvider
             theme={{
